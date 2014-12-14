3469517f14ffbbef922970c5c1040f4422b26e06
Masafumi Hidaka <c2849@st.cs.kumamoto-u.ac.jp>
2014-12-14 18:01:52 +0900

    Added html5

--- html5/timer.js
+++ html5/timer.js
@@ -1,14 +1,20 @@
+
 var myCnt = 0;
 var myTim = 0;
 
 function myGo(){
+//document.write("<p>abc</p>");
   mySelect = document.myForm.myMenu.selectedIndex;
+//document.write();
   myCnt = eval ( document.myForm.myMenu.options[mySelect].value );
+//document.write("<p>ggg</p>");
   myTim = setInterval ( "myTimer()", 1000 );
 };
 
 function myTimer(){
- document.wright("here");
+//document.open(); 
+document.write("<p>here</p>");
+//document.close();
   myCnt = myCnt - 1;
   document.getElementById( "countdown" ).innerHTML = "あと " + myCnt + " 秒 です。";
   if ( myCnt == 0 ){
@@ -27,16 +33,21 @@ onload = function(){
     goButton = document.getElementById("goButton");
     goButton.onclick = function(){
 	return myGo();
-    };
-   
-};
-
-
-onload = function(){
+  };
  var closeButton;
     closeButton = document.getElementById("closeButton");
     closeButton.onclick = function(){
-	 return window.close() ;
+	return window.close() ;
     };
    
 };
+
+
+/*onload = function(){
+ var closeButton;
+    closeButton = document.getElementById("closeButton");
+    closeButton.onclick = function(){
+	return window.close() ;
+    };
+   
+};*/
